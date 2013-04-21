3afe98ec060608a25d8bedaf9368b42d5b27c399
Tyler Schlegel <schleg@gmail.com>
2013-04-21 14:15:30 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandwagon::Application.config.secret_token = '17a017cf12d3387f86b2425cef18c0e6c8018c44500ba81966a4f76559bbc9bf5e79c1594d023155b8df9cde216760c7b811dedc0db447664c50e462a0cf47a9'
