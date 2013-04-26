8d1a006b77b3473fc668b9a3b87eceea5c2cf656
Tyler Schlegel <schleg@gmail.com>
2013-04-25 19:49:35 -0700

    adding state button labels

--- app/helpers/tshirts_helper.rb
+++ app/helpers/tshirts_helper.rb
@@ -1,2 +1,15 @@
 module TshirtsHelper
+
+  def state_button_label event
+    case event
+    when :submit
+      "Submit for Approval"
+    when :reject
+      "Reject Tee"
+    when :approve
+      "Approve Tee"
+    when :unpublish
+      "Unpublish Tee"
+    end
+  end
 end
